3ab81ac405d9dcef363d6a8d33b66a374e82c7d9
root <297155992@qq.com>
2019-08-19 14:23:10 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,6 @@ mv ~/.vimrc ~/.vimrc.bak >/dev/null 2>&1
 mv ~/.vim ~/.vim.bak > /dev/null 2>&1
 cp vimrc ~/.vimrc
 cp -r vim ~/.vim
+
+ctags -R --c++-kinds=+p --fields=+iaS --extra=+q -f ~/.systags /usr/include/glib-2.0/ /usr/include/linux/ /usr/include/c++/
+
